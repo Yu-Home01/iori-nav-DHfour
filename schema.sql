4b797f5e8f48e6b1e1901eba62b71f1a1b4d2bf4
Yu <412814589@qq.com>
2026-08-05 09:31:07 +0800

    feat: 数据库表新增 is_pinned 置顶字段

--- schema.sql
+++ schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS sites (
   catelog_name TEXT,
   sort_order INTEGER NOT NULL DEFAULT 9999,
   is_private INTEGER DEFAULT 0,
+  is_pinned INTEGER DEFAULT 0,
   create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   update_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
@@ -28,6 +29,7 @@ CREATE TABLE IF NOT EXISTS pending_sites (
   desc TEXT,
   catelog_id INTEGER NOT NULL,
   catelog_name TEXT,
+  is_pinned INTEGER DEFAULT 0,
   create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
